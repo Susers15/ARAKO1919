407738bf39e7fd968cd866fe3c41808fd2080e0e
Susers15 <tibita00815@gmail.com>
2024-10-24 21:05:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -175,7 +175,7 @@ function fadeOutBackgroundAndRedirect() {
     document.body.style.opacity = 0;
 
     setTimeout(() => {
-        window.location.href = "https://incandescent-syrniki-95478a.netlify.app//";
+        window.location.href = "https://arako2.netlify.app/";
     }, 1000);
 }
 
